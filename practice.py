32fb8b20ddb7af383443e71efdf1e3002d544211
Shayan Ghani <shayanghani1384@gmail.com>
2022-06-28 19:02:47 +0430

    script updated

--- practice.py
+++ practice.py
@@ -5,14 +5,13 @@
 from sys import exit
 
 
-
 def textMultiplier(text, number):
     if not text == "" or not number == "": 
-        if not number.isnumeric():
-            print("Enter a number...")
-            exit(1)    
-        else:
-            return text * int(number)
-    else:
         print("Don't leave it empty!") 
-        exit(1)
+        exit(1)    
+    elif number.isnumeric():
+        print("Enter a number...")
+        exit(1)    
+    else:
+        return text * int(number)
+        exit(0)

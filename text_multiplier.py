f24dda215bde2c34479e33bedeeaade27600d044
Shayan Ghani <shayanghani@gmail.com>
2022-07-12 19:50:22 +0430

    script updated!

--- text_multiplier.py
+++ text_multiplier.py
@@ -7,9 +7,9 @@ from sys import exit
 
 
 def textMultiplier(text, number):
-
-    text = input("Enter your peiece of text : ") + "/n"
-    number = input("How much : ")
+    # are used without testing
+    # text = input("Enter your peiece of text : ") + "\n"
+    # number = input("How much : ")
 
     if text == "" or number == "": 
         print("Don't leave it empty!")
